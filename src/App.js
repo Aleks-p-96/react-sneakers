8abb67d32c3a67a6db0ea9fefd1b47d22d49c64a
Aleks-p-96 <sanyapyrch1996@gmail.com>
2023-03-31 21:26:21 +0300

    l3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "./img/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "./img/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "./img/sneakers/3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "./img/sneakers/4.jpg",
+  },
+];
+
 
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer/>
-      <Header/>
+      <Drawer />
+      <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
@@ -18,7 +41,13 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
